3cb0140091e869674d3f3b453034db681632dc90
iliakuzmich <67041569+iliakuzmich@users.noreply.github.com>
2022-12-02 12:56:05 +0300

    тетрис готовый

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-#include "gameplay.h"
-#include "mainwindow.h"
-
+#include "widget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Widget w;
     w.show();
-
-    gameplay StartGame(w);
-
-    StartGame.StartTimer();
 
     return a.exec();
 }
